3f6a2d921a311176e11931c38d65071fccae495b
Marcos Vinícius <marcosviniciusmvmv7@gmail.com>
2024-01-29 21:51:08 -0300

    1.2
    Primeira classe do projeto

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using Tabuleiro;
+
+namespace XadrezConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao " + P);
+            Console.ReadLine();
+        }
+    }
+}
