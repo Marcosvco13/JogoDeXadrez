611710a90ca4a85ae9e22b790d7c9997889ad32e
Marcos Vinícius <marcosviniciusmvmv7@gmail.com>
2024-01-31 21:12:31 -0300

    1.6
    Ajustes na classe do tabuleiro. Utilizando sobrecarga e exceções.

--- XadrezConsole/Tabuleiro/Tabuleiro.cs
+++ XadrezConsole/Tabuleiro/Tabuleiro.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using Tabuleiro;
+
 namespace tabuleiro
 {
     class Tabuleiro
@@ -20,10 +21,38 @@ namespace tabuleiro
             return pecas[linhas, colunas];
         }
 
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if(pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }
